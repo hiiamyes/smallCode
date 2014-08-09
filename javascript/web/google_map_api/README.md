686e4ef28e3cf12a97bfa945eb990745102b9302
Yes <joshuayes@storysensecomputing.com>
2014-08-09 15:53:28 +0800

    add event listener and info window

---
+++ javascript/web/google_map_api/README.md
@@ -0,0 +1,5 @@
+feature:
+
+1. using html5 GeoLocation api for fetching current location
+1. map
+1. a simple marker + event listener + info window
--- javascript/web/google_map_api/js/google_map_api.js
+++ javascript/web/google_map_api/js/google_map_api.js
@@ -1,11 +1,11 @@
 function init() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(setGoogleMap, function(err){
-			console.log('getCurrentPosition err:' + err);
-		});
-    }else{
-		console.log('geolocation api err');
-	}
+        navigator.geolocation.getCurrentPosition(setGoogleMap, function(err) {
+            console.log('getCurrentPosition err:' + err);
+        });
+    } else {
+        console.log('geolocation api err');
+    }
 }
 
 function setGoogleMap(position) {
@@ -16,11 +16,21 @@ function setGoogleMap(position) {
     };
     var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
-    // To add the marker to the map, use the 'map' property
-    var myLatlng = new google.maps.LatLng(25.023779,121.549301);
     var marker = new google.maps.Marker({
-        position: myLatlng,
+        position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
         map: map,
-        title: "Hello World!"
+        title: 'Here I am'
+    });
+
+    var infoWindow = new google.maps.InfoWindow({
+        content: 'I\'m a info window'
+    });
+
+    google.maps.event.addListener(marker, 'mouseover', function() {
+        infoWindow.open(map, marker);
+    });
+
+    google.maps.event.addListener(marker,'mouseout', function(){
+        infoWindow.close();
     });
 }
