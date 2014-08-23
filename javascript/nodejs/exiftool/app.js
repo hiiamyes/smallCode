e9374a36129bd1e30770de28ec1d0319961f0184
Yes <joshuayes@storysensecomputing.com>
2014-08-23 09:49:08 +0800

    add a readme for exiftool

--- javascript/nodejs/exiftool/app.js
+++ javascript/nodejs/exiftool/app.js
@@ -1,6 +1,37 @@
-
 var cp = require('child_process');
+var readline = require('readline');
 
-cp.exec('exiftool photo.jpg', function(err, stdout, stderr) {
-  console.log(stdout);
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('What action?\n\
+	1:show keywords\n\
+	2:show geo\n\
+	3:add keyword\n\
+	4:remove keyword\n', function(answer) {
+    switch (answer) {
+        case '1':
+            cp.exec('exiftool -keywords photo.jpg', function(err, stdout, stderr) {
+                console.log(stdout);
+            });
+            break;
+        case '2':
+            cp.exec('exiftool -GPSPosition photo.jpg', function(err, stdout, stderr) {
+                console.log(stdout);
+            });
+            break;
+        case '3':
+            rl.question('keywords:', function(keyword) {
+                cp.exec('exiftool -keywords+=' + keyword + ' photo.jpg', function(err, stdout, stderr) {
+                    console.log(stdout);
+                });
+            });
+            break;
+        case '4':
+
+            break;
+    }
+    rl.close();
 });
---
+++ javascript/nodejs/exiftool/readme.md
@@ -0,0 +1,8 @@
+
+use child_process to run exiftool
+
+1. exiftool -[property_name]  [photo_name]
+	show property
+
+2. exiftool -[property_name]+= [property] [photo_name]
+	edit property
