abbb747aee88cb6afcb3a1d79e1fce3c4c190e6e
Yes <joshuayes@storysensecomputing.com>
2014-08-26 10:13:07 +0800

    add a post api

--- javascript/web/online_gps_editor/app.js
+++ javascript/web/online_gps_editor/app.js
@@ -1,10 +1,45 @@
 var express = require('express')
 var app = express()
+var cp = require('child_process');
+var bodyParser = require('body-parser');
 
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//
 app.use(express.static(__dirname));
 
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000);
+app.get('/api/gps', function(req, res) {
+    cp.exec('exiftool -GPSPosition img/photo.jpg', function(err, stdout, stderr) {
+        var stdout_splited = stdout.split(' ');
+        var lat =
+            parseFloat(stdout_splited[22]) +
+            parseFloat(stdout_splited[24].split('\'')[0] / 60) +
+            parseFloat(stdout_splited[25].split('\"')[0] / 3600);
+        var lng =
+            parseFloat(stdout_splited[27]) +
+            parseFloat(stdout_splited[29].split('\'')[0] / 60) +
+            parseFloat(stdout_splited[30].split('\"')[0] / 3600);
+        console.log(lat);
+        console.log(lng);
+        res.send({
+            'lat': lat,
+            'lng': lng
+        });
+    });
+});
+
+app.post('/api/gps', function(req,res){
+	console.log(req.body);
+});
+
+var port = 3000;
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
--- javascript/web/online_gps_editor/controller.js
+++ javascript/web/online_gps_editor/controller.js
@@ -1,55 +1,58 @@
 (function() {
     var app = angular.module('gps_editor', []);
 
-    app.controller('GPSEditorController', ['$scope',
-        function($scope) {
+    app.controller('GPSEditorController', ['$scope', '$http',
+        function($scope, $http) {
 
             var photo = {};
+            $scope.photo = photo;
+
             var geocoder = new google.maps.Geocoder();
             var map;
 
             photo.url = 'img/photo.jpg';
-            photo.lat = 1;
-            photo.lng = 1;
 
-            $scope.photo = photo;
+            $http({
+                method: 'GET',
+                url: '/api/gps'
+            }).success(function(data, status, headers, config) {
 
-            // $scope.initGoogleMap = function() {
-            //     console.log('init google map');
-            //     if (navigator.geolocation) {
-            //         navigator.geolocation.getCurrentPosition(setGoogleMap, function(err) {
-            //             console.log('getCurrentPosition err:' + err);
-            //         });
-            //     } else {
-            //         console.log('geolocation api err');
-            //     }
-            // }
-
-            $scope.setGoogleMap = function() {
-                console.log('set google map');
-                // var currentLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
                 var mapOptions = {
-                    center: new google.maps.LatLng(23.787701, 120.959473),
+                    center: (data !== {}) ? new google.maps.LatLng(data.lat, data.lng) : new google.maps.LatLng(23.787701, 120.959473),
                     zoom: 7,
                     mapTypeId: google.maps.MapTypeId.ROADMAP
                 };
                 map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
-                var marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(23.787701, 120.959473),
-                    map: map,
-                    title: 'I\'m a marker'
-                });
+                if (data !== {}) {
+                    photo.lat = data.lat;
+                    photo.lng = data.lng;
+                    var marker = new google.maps.Marker({
+                        position: new google.maps.LatLng(23.787701, 120.959473),
+                        map: map,
+                        title: 'I\'m a marker'
+                    });
+                };
 
                 google.maps.event.addListener(map, 'click', function(event) {
                     $scope.$apply(function() {
                         photo.lat = event.latLng.lat();
                         photo.lng = event.latLng.lng();
                         marker.setPosition(new google.maps.LatLng(event.latLng.lat(), event.latLng.lng()))
+                        $http.post('/api/gps', photo)
+                            .success(function(status) {
+                                console.log('success');
+                            })
+                            .error(function(status) {
+                                console.log('err')
+                            });
                     });
                 });
-            }
-            $scope.setGoogleMap();
+
+            }).error(function(data, status, headers, config) {
+                console.log('err');
+            });
+
 
             $scope.geocode = function() {
                 if ($scope.address) {
@@ -57,7 +60,7 @@
                         'address': $scope.address
                     }, function(results, status) {
                         if (status == google.maps.GeocoderStatus.OK) {
-                            map.setCenter(results[0].geometry.location);                            
+                            map.setCenter(results[0].geometry.location);
                         } else {
                             alert('Geocode was not successful for the following reason: ' + status);
                         }
