68124e12c80302d814bf9ddb694bc7cb91402011
Yes <joshuayes@storysensecomputing.com>
2014-08-30 20:26:44 +0800

    finish testing aws sdk for s3

---
+++ nodejs/aws_sdk_s3/app.js
@@ -0,0 +1,102 @@
+var aws = require('aws-sdk');
+var readline = require('readline');
+var fs = require('fs');
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+aws.config.loadFromPath('config.json');
+
+var s3 = new aws.S3();
+
+rl.question('action:', function(answer) {
+    switch (answer) {
+        case '1':
+            createBucket();
+            break;
+        case '2':
+            deleteBucket();
+            break;
+        case '3':
+            uploadPhoto();
+            break;
+        case '4':
+            listPhoto();
+            break;
+        case '5':
+            deletePhoto();
+            break;
+    }
+});
+
+function createBucket() {
+    s3.createBucket({
+        Bucket: 'hiiamyestestbbb'
+    }, function(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('bucket created');
+        }
+    });
+}
+
+function deleteBucket() {
+    var params = {
+        Bucket: 'hiiamyestestbbb'
+    };
+    s3.deleteBucket(params, function(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('bucket deleted');
+        }
+    });
+}
+
+function uploadPhoto() {
+    fs.readFile('photo.JPG', function(err, data) {
+        var params = {
+            Bucket: 'hiiamyestestbbb',
+            Key: 'photo1',
+            Body: data,
+            ContentType: 'image/jpeg'
+        };
+        s3.putObject(params, function(err, data) {
+            if (err) {
+                console.log('err' + err);
+            } else {
+                console.log('success' + data);
+            }
+        });
+    });
+}
+
+function listPhoto() {
+    var params = {
+        Bucket: 'hiiamyestestbbb'
+    };
+    s3.listObjects(params, function(err, data) {
+        if (err) {
+            console.log('err' + err);
+        } else {
+            console.log(data);
+        }
+    });
+}
+
+function deletePhoto() {
+    var params = {
+        Bucket: 'hiiamyestestbbb',
+        Key: 'photo1'
+    };
+    s3.deleteObject(params, function(err, data) {
+        if (err) {
+            console.log('err' + err);
+        } else {
+            console.log(data);
+        }
+    });
+}
